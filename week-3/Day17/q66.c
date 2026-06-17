1b571ff3960d9c91c6fd68bb265a705e2f7cc1a5
Aanjaneya <aanjaneyasharma1999@gmail.com>
2026-06-17 17:40:26 +0530

    Create q66.c

---
+++ week-3/Day17/q66.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char str[100];
+
+    printf("Enter string: ");
+    gets(str);
+
+    strrev(str);
+
+    printf("Reversed String = %s", str);
+
+    return 0;
+}
