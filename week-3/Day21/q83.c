0dd61c29ec688c68b06aec9bc0acc99f67dbd0a2
Aanjaneya <aanjaneyasharma1999@gmail.com>
2026-06-21 22:06:26 +0530

    Create q83.c

---
+++ week-3/Day21/q83.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+struct Student {
+    int roll;
+    char name[50];
+};
+
+int main() {
+    struct Student s[3];
+
+    for(int i = 0; i < 3; i++) {
+        printf("Enter Roll and Name: ");
+        scanf("%d %s", &s[i].roll, s[i].name);
+    }
+
+    printf("\nStudent Records\n");
+
+    for(int i = 0; i < 3; i++) {
+        printf("%d %s\n", s[i].roll, s[i].name);
+    }
+
+    return 0;
+}
