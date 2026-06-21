9eb3bc964aa01aa9e6232f8b8f06ca24fcf5876a
Aanjaneya <aanjaneyasharma1999@gmail.com>
2026-06-21 22:00:09 +0530

    Create q76.c

---
+++ week-3/Day19/q76.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() {
+    char str[100], ch;
+    int count = 0;
+
+    printf("Enter string: ");
+    gets(str);
+
+    printf("Enter character: ");
+    scanf("%c", &ch);
+
+    for(int i = 0; str[i] != '\0'; i++) {
+        if(str[i] == ch)
+            count++;
+    }
+
+    printf("Frequency = %d", count);
+
+    return 0;
+}
