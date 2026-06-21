31429b6908ede379d4eee307b644f9e6ec5d94ac
Aanjaneya <aanjaneyasharma1999@gmail.com>
2026-06-21 22:03:10 +0530

    Add program to calculate sum of two numbers

---
+++ week-3/Day20/q79.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    int a, b, *p1, *p2;
+
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+
+    p1 = &a;
+    p2 = &b;
+
+    printf("Sum = %d", *p1 + *p2);
+
+    return 0;
+}
